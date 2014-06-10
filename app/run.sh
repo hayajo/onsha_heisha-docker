16dafadeb72a0af86dc48a7707551f8d13da721e
hayajo <hayajo@cpan.org>
2014-06-10 16:01:48 +0900

    Refactored

--- app/run.sh
+++ app/run.sh
@@ -2,8 +2,6 @@
 
 /usr/sbin/sshd
 
-perl -i -ple "s|^(\\s*add_action\\('template_redirect', 'redirect_canonical'\\);\\s*)\$|//\$1|" /var/www/html/wp-includes/canonical.php
-
 DB_HOST=$DB_PORT_3306_TCP_ADDR LB_PORT=8080 perl -i -ple 's|\{\{\s*(.+?)\s*}}|$ENV{$1}|ge' /var/www/html//wp-config.php
 
 /usr/sbin/httpd
